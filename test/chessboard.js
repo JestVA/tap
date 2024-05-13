6c6198d17156dd98a0fa152bf94fde7f7256b88c
devfrend <d.dumitrascuta@gmail.com>
2024-05-13 23:33:47 +0100

    A chessboard test

---
+++ test/chessboard.js
@@ -0,0 +1,45 @@
+import test from "tape"
+
+function chessboard(size) {
+  let board = "\n"
+  let row = 0
+  for (let i = 1; i <= Math.pow(size, 2); i++) {
+    if (row % 2 === 0) {
+      if (i % 2 === 0) {
+        board += "#"
+      } else {
+        board += " "
+      }
+    }
+    if (row % 2 !== 0) {
+      if (i % 2 === 0) {
+        board += " "
+      } else {
+        board += "#"
+      }
+    }
+    if (i % size === 0) {
+      board += "\n"
+      row += 1
+    }
+  }
+  return board
+}
+
+const renderChessboardString = `
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+`
+
+test("Chessboard", (t) => {
+  t.plan(1)
+  t.equal(chessboard(8), renderChessboardString)
+})
+
+console.log(chessboard(8))
