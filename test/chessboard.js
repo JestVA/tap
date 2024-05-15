da06c56ac1e71e9ddc5d4b4efbdcc497b6d40c61
devfrend <d.dumitrascuta@gmail.com>
2024-05-15 09:40:31 +0100

    extract instructions to render a chessboard row to string

--- test/chessboard.js
+++ test/chessboard.js
@@ -1,22 +1,22 @@
 import test from "tape"
 
+const renderString = (i, invert) => {
+  if (i % 2 === 0) {
+    return invert ? " " : "#"
+  } else {
+    return invert ? "#" : " "
+  }
+}
+
 function chessboard(size) {
   let board = "\n"
   let row = 0
   for (let i = 1; i <= Math.pow(size, 2); i++) {
     if (row % 2 === 0) {
-      if (i % 2 === 0) {
-        board += "#"
-      } else {
-        board += " "
-      }
+      board += renderString(i)
     }
     if (row % 2 !== 0) {
-      if (i % 2 === 0) {
-        board += " "
-      } else {
-        board += "#"
-      }
+      board += renderString(i, true)
     }
     if (i % size === 0) {
       board += "\n"
