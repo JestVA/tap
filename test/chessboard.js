9769d30053a3e553ca5d4d8d78e392aa34ae367b
devfrend <d.dumitrascuta@gmail.com>
2024-05-15 10:10:39 +0100

    split into more functional parts

--- test/chessboard.js
+++ test/chessboard.js
@@ -1,22 +1,28 @@
 import test from "tape"
 
-const renderString = (i, invert) => {
-  if (i % 2 === 0) {
-    return invert ? " " : "#"
+const renderString = (i, isCheckered) => {
+  const isEven = i % 2 === 0
+  if (isEven) {
+    return isCheckered ? " " : "#"
   } else {
-    return invert ? "#" : " "
+    return isCheckered ? "#" : " "
   }
 }
+
+const addNewLineOnRowEnd = (rowEnd) => (rowEnd ? "\n" : "")
+
+const incrementRow = (increment, row) => (increment ? row + 1 : row)
 
 const chessboard = (size) => {
   let board = "\n"
   let row = 0
+
   for (let i = 1; i <= Math.pow(size, 2); i++) {
-    board += renderString(i, row % 2 !== 0)
-    if (i % size === 0) {
-      board += "\n"
-      row += 1
-    }
+    const isRowEnd = i % size === 0
+    const isCheckered = row % 2 !== 0
+    board += renderString(i, isCheckered)
+    board += addNewLineOnRowEnd(isRowEnd)
+    row = incrementRow(isRowEnd, row)
   }
   return board
 }
