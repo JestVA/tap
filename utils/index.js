815034adf8c1646fa9b0be86c9e0cf75e0838a98
devfrend <d.dumitrascuta@gmail.com>
2024-05-18 22:01:14 +0100

    added pipe and partial application function utils

--- utils/index.js
+++ utils/index.js
@@ -1,1 +1,7 @@
 export const compose = (input, fn) => fn(input)
+export const partialPrefill = (fn, prefill) => (liveInput) =>
+  fn(liveInput, prefill)
+export const pipe =
+  (...fns) =>
+  (x) =>
+    fns.reduce((v, f) => f(v), x)
