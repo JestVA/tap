174a997e5f3d66f4c94c58d6e8292b1ebe17f6c2
devfrend <d.dumitrascuta@gmail.com>
2024-06-11 22:36:59 +0100

    reasoning through this

--- test/word-wrapper.js
+++ test/word-wrapper.js
@@ -1,6 +1,48 @@
 import test from "tape"
 import { isAlphanumeric } from "../utils/index.js"
 
+/**
+ * 
+       VERY difficult to traverse and reason about what's happening here
+ */
+
+       // Assert tests
+
+       test("Breaks words to space", (t) => {
+
+        function trim(text, lineLength) {
+            // how does it deal with the edges?
+            console.log(text)
+        }
+
+        t.plan(2)
+        
+        const t1 = trim("There goes, restroom is empty.", 10)
+        const t2 = trim("Hello, world! I am hungry.", 10)
+
+        t.assert(t1)
+        t.assert(t2)
+
+       })
+
+
+
+       test("Can wrap words", (t) => {
+        // test with long and short input cases
+       })
+
+        test("Wraps text to line length", (t) => {
+            // unknown
+        })
+
+        test("Dashes are special", (t) => {
+            // broken words get converted to dashes
+        })
+
+        test("Line does not start with non-alphanumeric character", (t) => {})
+
+
+
 const wrap = (text, length) => {
   if (text.length <= length) return text
   let lineLength = 0
