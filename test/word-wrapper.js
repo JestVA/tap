202aabce5932738fc58e99d57d895bd3a6ab5b7d
devfrend <d.dumitrascuta@gmail.com>
2024-06-18 14:58:55 +0100

    add ts config and tsx, remove ts-node as dependency

--- test/word-wrapper.js
+++ test/word-wrapper.js
@@ -1,30 +1,40 @@
 import test from 'tape';
 import { isAlphanumeric } from '../utils/index.js';
 
-function wrap(text, length) {
-  if (length === 0) return '';
-  if (!text) return '';
-  if (length === undefined) return text;
+//function wrap(text, length) {
+//  if (length === 0) return '';
+//  if (!text) return '';
+//  if (length === undefined) return text;
 
-  let currentWrapped = '';
+//  const tokens = text.split(' ');
 
-  for (let letter of text) {
-    console.log('here ->', letter);
-    if (currentWrapped.length % length === 0) {
-      currentWrapped += '\n';
-    }
+//  const render = tokens.reduce((renderString, token) => {
+//    const multiplier = renderString.split('\n').length;
 
-    //if (currentWrapped.length + 1 === length) {
-    //  currentWrapped += '-';
-    //  currentWrapped += '\n';
-    //}
+//    const spaceLeft = multiplier * length - renderString.length;
+//    console.log(spaceLeft);
+//    if (spaceLeft >= token.length) {
+//      renderString += token + ' ';
+//      console.log('rendered', token);
+//    } else {
+//    }
 
-    currentWrapped += letter;
-  }
+//    return renderString;
+//  }, '');
 
-  console.log('Return result is: ', currentWrapped);
-  return currentWrapped;
-}
+//  //  for (let letter of text) {
+//  //    console.log('here ->', letter);
+//  //    currentWrapped += letter;
+//  //    // append new line
+//  //    if (currentWrapped.length % length === 0) {
+//  //      currentWrapped += '\n';
+//  //    }
+//  //  }
+
+//  console.log('render is: ', render);
+
+//  return render;
+//}
 
 // information has to have a secondary semiotic
 // technical, beyond interpretable dispute
@@ -88,52 +98,101 @@ function wrap(text, length) {
 //  return rows[rows.length - 1];
 //}
 
-// THE MOST BASIC TESTS - does the `f` return anything useful? Does it work? Is the return type the expected type?
-test('Returns a string', (t) => {
-  t.plan(4);
-  t.equal(
-    typeof wrap('This is a string', 1),
-    'string',
-    `Function returns a string value when called with arguments`
-  );
-  t.equal(
-    wrap(undefined, undefined),
-    '',
-    `Returns an empty string if text is not passed to function`
-  );
-  t.equal(
-    wrap('Another test case', undefined),
-    'Another test case',
-    `Returns original string when length is not passed`
-  );
-  t.equal(
-    typeof wrap('Demo line', 0),
-    'string',
-    `Truncates to empty string when passed length is 0`
-  );
-});
+//// THE MOST BASIC TESTS - does the `f` return anything useful? Does it work? Is the return type the expected type?
+//test('Returns a string', (t) => {
+//  t.plan(4);
+//  t.equal(
+//    typeof wrap('This is a string', 1),
+//    'string',
+//    `Function returns a string value when called with arguments`
+//  );
+//  t.equal(
+//    wrap(undefined, undefined),
+//    '',
+//    `Returns an empty string if text is not passed to function`
+//  );
+//  t.equal(
+//    wrap('Another test case', undefined),
+//    'Another test case',
+//    `Returns original string when length is not passed`
+//  );
+//  t.equal(
+//    typeof wrap('Demo line', 0),
+//    'string',
+//    `Truncates to empty string when passed length is 0`
+//  );
+//});
 
-test('Line wrapping', (t) => {
-  const lengthArg = 5;
-  const result = wrap('The maximum length of any line here is 5', lengthArg);
-  const returnedLength = result.split('\n')[0].length;
-  t.plan(1);
-  t.assert(
-    returnedLength <= lengthArg,
-    `Line length does not exceed wrapping length argument. Returned length was |-- ${returnedLength}. Requirement |-- ${lengthArg}`
-  );
-});
+//test('Line wrapping', (t) => {
+//  const lengthArg = 5;
+//  const result = wrap('The maximum length of any line here is 5', lengthArg);
+//  const lines = result.split('\n');
+//  t.plan(lines.length);
+//  for (const test of lines) {
+//    t.assert(
+//      test.length <= lengthArg,
+//      `Line length does not exceed wrapping length argument. Returned length was |-- ${test.length}. Requirement |-- ${lengthArg}`
+//    );
+//  }
+//});
 
-test('How do we trust the result', (t) => {
-  // need to prove false that a program might not show all information
+//test('How do we trust the result', (t) => {
+//  // need to prove false that a program might not show all information
+//  const original = 'Hello, world! I am hungry.';
+//  const snapshot = `Hello, wo-
+//  rld! I am
+//  hungry.`;
+//  const wrapped = wrap(original, 10);
+//  t.plan(1);
+//  t.assert(
+//    wrapped.length === snapshot.length,
+//    `Result formatting matches snapshot: Result|--${wrapped.length} Original|--${snapshot.length}`
+//  );
+//});
+
+class Wrapper {
+  constructor(text, length) {
+    this.text = text;
+    this.length = length;
+  }
+
+  isValidChar() {
+    const isValid = this.text
+      .split('\n')
+      .every((chunk) => isAlphanumeric(chunk[0]));
+    return isValid;
+  }
+
+  insertBreakWordCharacter() {
+    return (this.text += '-');
+  }
+
+  insertSpace() {
+    console.log(this.text);
+    return (this.text += ' ');
+  }
+
+  insertNewLineCharacter() {
+    return (this.text += '\n');
+  }
+
+  parse() {
+    console.log(this.text);
+    return this.text;
+  }
+}
+
+test('Output format', (t) => {
   const original = 'Hello, world! I am hungry.';
-  const snapshot = `Hello, wo-
-  rld! I am 
-  hungry.`;
-  const wrapped = wrap(original, 10);
+  const snapshot = `Hello, wo-\nrld! I am \nhungry.`;
+  const wrapper = new Wrapper(original, 10);
+  const parsedLength = wrapper.parse().length;
+  const snapshotLength = snapshot.length;
   t.plan(1);
   t.assert(
-    wrapped.length === snapshot.length,
-    `Result formatting matches snapshot: Result|--${wrapped.length} Original|--${snapshot.length}`
+    parsedLength === snapshotLength,
+    `Formatting matches snapshot: 
+     Result|--${parsedLength} Original|--${snapshotLength}`
   );
+  console.log(`Snapshot\n${snapshot}`);
 });
